f777de4a4c1d91868cc95ca20ac926d97dedef7b
TakashiMonno <sesirou@gmail.com>
2022-04-13 14:47:45 +0900

    02

---
+++ W02/Vec3.js
@@ -0,0 +1,19 @@
+class Vec3
+{
+    // Constructor
+    constructor( x, y, z )
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    
+    add( v )
+        {
+        this.x += v.x;
+        this.y += v.y;
+        this.z += v.z;
+        return this;
+        }
+}
+
