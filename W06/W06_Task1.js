6006f1acaef813b4349afaecbe9d69a73b658fb8
TakashiMonno <sesirou@gmail.com>
2022-05-06 16:42:37 +0900

    06

---
+++ W06/W06_Task1.js
@@ -0,0 +1,100 @@
+d3.csv("W06_Task1.csv")
+    .then( data => {
+        data.forEach( d => { d.x = +d.x; d.y = +d.y; });
+
+        var config = {
+            parent: '#drawing_region',
+            width: 256,
+            height: 256,
+            margin: {top:30, right:30, bottom:30, left:30}
+        };
+
+        const scatter_plot = new ScatterPlot( config, data );
+        scatter_plot.update();
+    })
+    .catch( error => {
+        console.log( error );
+    });
+
+class ScatterPlot {
+
+        constructor( config, data ) {
+            this.config = {
+                parent: config.parent,
+                width: config.width || 256,
+                height: config.height || 256,
+                margin: config.margin || {top:10, right:10, bottom:10, left:10}
+            }
+            this.data = data;
+            this.init();
+        }
+    
+        init() {
+            let self = this;
+
+            //svgのサイズ設定
+            self.svg = d3.select( self.config.parent )
+                .attr('width', self.config.width)
+                .attr('height', self.config.height);
+    
+            self.xchart = self.svg.append('g')
+                .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+            
+            self.ychart = self.svg.append('g')
+                .attr('transform', `translate(${self.config.margin.right}, -165)`);
+    
+            self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+            self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+
+            // スケールの調整
+            self.xscale = d3.scaleLinear()
+                .range( [0, self.inner_width] );
+    
+            self.yscale = d3.scaleLinear()
+                .range( [0, self.inner_height] );
+    
+            self.xaxis = d3.axisBottom( self.xscale )
+                .ticks(6);
+    
+            self.xaxis_group = self.xchart.append('g')
+                .attr('transform', `translate(0, ${self.inner_height})`);
+            
+            self.yaxis = d3.axisLeft( self.yscale )
+                .ticks(6);
+    
+            self.yaxis_group = self.ychart.append('g')
+                .attr('transform', `translate(0,${self.inner_width})`);
+        }
+    
+        update() {
+            let self = this;
+    
+            const xmin = d3.min( self.data, d => d.x );
+            const xmax = d3.max( self.data, d => d.x );
+            self.xscale.domain( [0, xmax] );
+    
+            const ymin = d3.min( self.data, d => d.y );
+            const ymax = d3.max( self.data, d => d.y );
+            self.yscale.domain( [ymax, 0] );
+    
+            self.render();
+        }
+    
+        render() {
+            let self = this;
+    
+            self.xchart.selectAll("circle")
+                .data(self.data)
+                .enter()
+                .append("circle")
+                .attr("cx", d => self.xscale( d.x ) )
+                .attr("cy", d => self.yscale( d.y ) )
+                .attr("r", d => d.r );
+    
+            self.xaxis_group
+                .call( self.xaxis );
+            
+            self.yaxis_group
+                .call( self.yaxis );
+        }
+    }
