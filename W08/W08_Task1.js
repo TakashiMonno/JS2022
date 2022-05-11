b0dc29f21082d68174056f6e4d1d2fd805859b8e
TakashiMonno <sesirou@gmail.com>
2022-05-11 15:26:48 +0900

    08

--- W08/W08_Task1.js
+++ W08/W08_Task1.js
@@ -1,12 +1,12 @@
-d3.csv("https://takashimonno.github.io/JS2022//W08/W08_data.csv")
+d3.csv("W08_data.csv")
     .then( data => {
-        data.forEach( d => { d.width = +d.value;});
+        data.forEach( d => { d.value = +d.value;});
 
         var config = {
             parent: '#drawing_region',
             width: 256,
-            height: 256,
-            margin: {top:30, right:30, bottom:30, left:30},
+            height: 128,
+            margin: {top:10, right:10, bottom:20, left:60},
         };
 
         const barchart = new Barchart( config, data );
@@ -20,9 +20,10 @@ class Barchart {
 
         constructor( config, data ) {
             this.config = {
+                parent: config.parent,
                 width: config.width || 256,
-                height: config.height || 256,
-                margin: config.margin || {top:10, right:10, bottom:10, left:10},
+                height: config.height || 128,
+                margin: config.margin || {top:10, right:10, bottom:20, left:60}
             }
             this.data = data;
             this.init();
@@ -37,26 +38,29 @@ class Barchart {
             //svgのサイズ設定
             self.svg = d3.select( self.config.parent )
                 .attr('width', self.config.width)
-                .attr('height', self.config.height+self.config.margin.top);
+                .attr('height', self.config.height);
     
             self.chart = self.svg.append('g')
                 .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
 
             // スケールの調整
             self.xscale = d3.scaleLinear()
+                            .domain( [0, d3.max(self.data, d => d.value)] )
                             .range([0, self.inner_width]);
+            self.yscale = d3.scaleBand()
+                            .domain(self.data.map(d => d.label))
+                            .range([0, self.inner_height])
+                            .paddingInner(0.1);
+
             self.xaxis = d3.axisBottom( self.xscale )
                             .ticks(5)
                             .tickSizeOuter(0);
-            self.xaxis_group = self.chart.append('g')
-                                .attr('transform', `translate(0, ${self.inner_height})`)
-                                .call(self.xaxis)
-            
-            self.yscale = d3.scaleBand()
-                            .range([0, self.inner_height])
-                            .paddingInner(0.1);
             self.yaxis = d3.axisLeft( self.yscale )
                             .tickSizeOuter(0);
+
+            self.xaxis_group = self.chart.append('g')
+                                .attr('transform', `translate(0, ${self.inner_height})`)
+                                .call(self.xaxis);
             self.yaxis_group = self.chart.append('g')
                                 .call(self.yaxis);
         }
@@ -64,10 +68,9 @@ class Barchart {
         update() {
             let self = this;
     
-            const xmax = d3.max( self.data, d => d.value );
-            self.xscale.domain( [0, xmax] );
+            self.xscale.domain( [0, d3.max(self.data, d => d.value)] );
 
-            self.yscale.dmain(self.data.map(d => self.config.label));
+            self.yscale.domain(self.data.map(d => d.label));
     
             self.render();
         }
@@ -75,12 +78,12 @@ class Barchart {
         render() {
             let self = this;
 
-            chart.selectAll("rect").data(self.data).enter()
+            self.chart.selectAll("rect").data(self.data).enter()
                 .append("rect")
                 .attr("x", 0)
-                .attr("y", d => yscale(d.label))
-                .attr("width", d => xscale(d.value))
-                .attr("height", d.yscale.bandwidth());
+                .attr("y", d => self.yscale(d.label))
+                .attr("width", d => self.xscale(d.value))
+                .attr("height", self.yscale.bandwidth());
 
         }
     }
